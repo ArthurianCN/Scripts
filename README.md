fc05537d14f1eb0833e64d8e0dc3bbe55f96b19f
unknown <zhuanxinxin@aliyun.com>
2023-07-19 22:49:31 +0800

    Monitor multi process

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # Scripts
+Some useful scripts.
--- bash/monitor.sh
+++ bash/monitor.sh
@@ -1,3 +1,4 @@
+
 #!/bin/bash
 
 procname="$1"
@@ -6,19 +7,34 @@ logfile="$logpath/memusage.log"
 interval=3
 mkdir -p $logpath
 
+function log_memusage()
+{
+	pid=$1
+	memusage_percent=$(ps -p $pid -o %mem | tail -n 1)
+	memusage_bytes=$(ps -p $pid -o rss | tail -n 1)
+	timestamp=$(date +%Y-%m-%d_%H:%M:%S)
+	echo "$timestamp [pid:$pid] mem:$memusage_bytes KB"
+	echo "$timestamp [pid:$pid] mem:$memusage_bytes KB" >> $logfile
+}
+
+
 while true
 do
-	pid=$(pidof $procname) 
-	if [ -z "$pid" ]; then
+	pid_list=$(pidof $procname) 
+	if [ -z "$pid_list" ]; then
 		echo "$procname not running"
 		sleep 1
 		continue
 	fi
-	memusage_percent=$(ps -p $pid -o %mem | tail -n 1)
-	memusage_kb=$(ps -p $pid -o rss | tail -n 1) 
-	timestamp=$(date +%Y-%m-%d_%H:%M:%S)
-	echo "[pid:$pid] $timestamp mem:$memusage_kb KB" 
-	echo "[pid:$pid] $timestamp mem:$memusage_kb KB" >> $logfile
+
+	oldIFS=$IFS
+	
+	IFS=' '
+	for item in $pid_list; do
+		log_memusage "$item"
+	done
+
+	IFS=$oldIFS
     
 	sleep $interval
 done
